25c891398c183ea0f21d91d6a609f10e713696fa
Patrick Maweu <muilipato@gmail.com>
2023-03-29 00:41:00 +0300

    Looping lab done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+// Code your solutions in this file
+const writeCards = (array, event) => {
+    let myArray = []
+    let name = "";
+
+    for (name of array) {
+        myArray.push(`Thank you, ${name}, for the wonderful ${event} gift!`)
+    }
+    return myArray
+
+}
+
+const countDown = (number) => {
+    for (let i = number; i >= 0; i--) {
+        console.log(i)
+    }
+}
